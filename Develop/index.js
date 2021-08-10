02789408674fb9068023c267699b2bf71b714905
God of War <jamel.eggleston@gmail.com>
2021-08-09 23:17:59 -0400

    Added packages and questions to index.js file.

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,40 @@
-// TODO: Include packages needed for this application
+// all of my packages. 
+// The inquirer package is needed to get the users input from the terminal.
+const inquirer = require('inquirer');
+// The fs package is needed to write the README.md file to the file system.
+const fs = require('fs');
+const generateMarkdown = require('generate-markdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type:"input",
+        message: "Enter a title for your project",
+        name: "title"
+
+    },
+    {
+        type:"input",
+        message: "Enter a description for your project",
+        name: "description",
+    },
+    {
+        type:"input",
+        message: "Enter Project installation instructions",
+        name: "install"
+    },
+    {
+        type:"input",
+        message: "Enter project usage information",
+        name: "usage",
+    },
+    {
+        type:"input",
+        message: "Enter project test information",
+        name: "test",
+    },
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
